4e2c83a7a25d5d5327f038c9a91b1c10f4671f3a
Stefano Cecere <dev@krur.com>
2014-01-24 00:06:48 +0100

    0.1.6 clear caches docs

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ pressenza-plugin
 ================
 
 pressenza.com main plugin
+
+
+
+clean update cache:
+update wp_options set option_value='' where option_name='_site_transient_update_plugins';
--- pressenza-plugin.php
+++ pressenza-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name:       Pressenza
 Plugin URI:        https://github.com/Pressenza/pressenza-plugin
-Version:           0.1.5
+Version:           0.1.6
 Description:       collection of widgets and setup for pressenza.com
 Author:            Stefano Cecere & Tom Butikofer
 Author URI:        https://github.com/Pressenza
