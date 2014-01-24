0420be4737191871a48c98fdbb3c148186acae42
Stefano Cecere <dev@krur.com>
2014-01-24 12:49:42 +0100

    0.1.9.1

--- pressenza-plugin.php
+++ pressenza-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name:       Pressenza
 Plugin URI:        https://github.com/Pressenza/pressenza-plugin
-Version:           0.1.9
+Version:           0.1.9.1
 Description:       collection of widgets and setup for pressenza.com
 Author:            Stefano Cecere & Tom Butikofer
 Author URI:        https://github.com/Pressenza
